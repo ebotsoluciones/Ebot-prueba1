83378dbd4db12c434096d4f73054ff4c1183f30b
ebotsoluciones <ebotsoluciones@gmail.com>
2026-03-25 19:34:36 -0300

    Add configuration settings to app/config.py

---
+++ app/config.py
@@ -0,0 +1,10 @@
+ADMINS = ["whatsapp:+5493515645624"]
+
+HORARIO_INICIO = "09:00"
+HORARIO_FIN = "19:00"
+INTERVALO = 30
+
+TURNOS_FILE = "data/turnos.json"
+MENSAJES_FILE = "data/mensajes.json"
+BLOQUEOS_FILE = "data/bloqueos.json"
+ESTADO_FILE = "data/estado.json"
