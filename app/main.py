7405c26e768c42fc5344ba23a60f8cdd66fae907
ebotsoluciones <ebotsoluciones@gmail.com>
2026-03-27 01:20:14 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
+
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from bot import procesar
@@ -17,6 +21,5 @@ def home():
     return "E-Bot activo ✅"
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))   # Railway inyecta PORT automáticamente
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
